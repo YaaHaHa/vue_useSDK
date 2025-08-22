437434941f45266f71693ec19885eacfa4efee86
YaaHaHa <1902793546@qq.com>
2025-08-22 22:25:22 +0800

    定位到错误源文件，还原code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from "vue";
 import ErrorStackParser from "error-stack-parser";
+import { getOriginalCode } from "./utils";
 import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.config.errorHandler = (err, vm) => {
+  // 解析错误获得错误栈
   const ErrorStack = ErrorStackParser.parse(err as Error);
-  console.error("err", err, "ErrorStack", ErrorStack, "vm", vm);
+  //   console.error("err", err, "ErrorStack", ErrorStack, "vm", vm);
+  getOriginalCode(ErrorStack[0]);
 };
 
 app.mount("#app");
---
+++ src/utils/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import SouceMap from "source-map-js";
+const MAP_URL = "";
+
+// 获取报错源文件
+export async function getOriginalCode(stackFrame: any) {
+  debugger;
+  const sourcemap = await axios.get(`${MAP_URL}/${stackFrame.fileName}.map`);
+  const fileContent = sourcemap.data;
+  // 解析map文件
+  const smc = await new SouceMap.SourceMapConsumer(fileContent);
+  // 通过报错的位置还原
+  const originalPosition = smc.originalPositionFor({
+    line: stackFrame.lineNumber,
+    column: stackFrame.columnNumber,
+  });
+  // 获取真实错误代码
+  const originalCode = smc.sourceContentFor(originalPosition.source);
+  console.log("还原后的错误代码", originalCode);
+  return originalCode;
+}
